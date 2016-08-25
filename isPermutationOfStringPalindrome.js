c214da012ddafd0f428e260fb39f182f5b9463c6
Hanuman Sahay jat <hanuman.jat@askme.in>
2016-08-25 11:42:51 +0530

    permutation of an input string is a palindrome

---
+++ isPermutationOfStringPalindrome.js
@@ -0,0 +1,18 @@
+function canRearrangeToPalindrome(str)
+{
+    var letterCounts = {};
+    var letter;
+    var palindromeSum = 0;
+    for (var i = 0; i < str.length; i++) {
+        letter = str[i];
+        letterCounts[letter] = letterCounts[letter] || 0;
+        letterCounts[letter]++;
+    }
+    for (var letterCount in letterCounts) {
+        palindromeSum += letterCounts[letterCount] % 2;
+    }
+
+    return palindromeSum < 2;
+}
+
+console.log(canRearrangeToPalindrome("civci"));
